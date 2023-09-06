6a82bf49944c39d5c102719a284b8a82764d26a3
Saimun Hassan <saimunhas@gmail.com>
2023-09-05 21:04:32 -0400

    Added functionality for displaying feedback on sending review

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const rating = document.getElementsByClassName('rating');
+const rating = document.querySelector('.rating');
+const btn = document.getElementById('btn');
+const container = document.getElementById('container');
+
+rating.addEventListener('click', () => {
+  rating.classList.toggle('active');
+});
+
+btn.addEventListener('click', () => {
+  let content = rating.childNodes[2].textContent;
+  container.innerHTML = `<h3>Thank you!</h3>
+  <h3>Feedback : ${content}</h3
+  <p>We'll use your feedback to improve our customer support.</p>
+  `;
+});
