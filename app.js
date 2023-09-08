2732519dba885fc59682ed63f799f7a07571a095
Saimun Hassan <saimunhas@gmail.com>
2023-09-07 20:33:21 -0400

    Added functionality for adding active class

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const rating = document.querySelector('.rating');
+const ratingEls = document.querySelectorAll('.rating');
 const btn = document.getElementById('btn');
 const container = document.getElementById('container');
 
-rating.addEventListener('click', () => {
-  rating.classList.toggle('active');
+ratingEls.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    // console.log(e.target.innerText || e.target.parentNode.innerText);
+    console.log(e.target.innerText);
+    e.target.classList.add('active');
+    e.target.parentNode.classList.toggle('active');
+  });
 });
+
+// rating.addEventListener('click', () => {
+//   rating.classList.toggle('active');
+//   if (rating.classList.contains('!active')) {
+//     rating.classList.add('active');
+//   } else {
+//     rating.classList.remove('active');
+//   }
+// });
 
 btn.addEventListener('click', () => {
   let content = rating.childNodes[2].textContent;
