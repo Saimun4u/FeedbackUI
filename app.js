c2db8bd2d3d888bd4400078f4e09f305b0e08936
Saimun Hassan <saimunhas@gmail.com>
2023-09-07 20:58:55 -0400

    Added feedback message after button is clicked

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const ratingEls = document.querySelectorAll('.rating');
 const btn = document.getElementById('btn');
 const container = document.getElementById('container');
 
+let checkedRating = '';
+
 ratingEls.forEach((item) => {
   item.addEventListener('click', (e) => {
     // console.log(e.target.innerText || e.target.parentNode.innerText);
-    console.log(e.target.innerText);
+    removeActive();
+    checkedRating = e.target.innerText;
     e.target.classList.add('active');
     e.target.parentNode.classList.toggle('active');
   });
 });
+
+function removeActive() {
+  ratingEls.forEach((item) => {
+    item.classList.remove('active');
+  });
+}
 
 // rating.addEventListener('click', () => {
 //   rating.classList.toggle('active');
@@ -21,9 +30,13 @@ ratingEls.forEach((item) => {
 // });
 
 btn.addEventListener('click', () => {
-  let content = rating.childNodes[2].textContent;
-  container.innerHTML = `<h3>Thank you!</h3>
-  <h3>Feedback : ${content}</h3
-  <p>We'll use your feedback to improve our customer support.</p>
-  `;
+  if (checkedRating != '') {
+    container.innerHTML = `<strong>Thank You!</strong>
+    <br>
+    <br>
+    <strong>Feedback : ${checkedRating}</strong>
+    <p>We'll use your feedback to improve our customer support.</p>
+    `;
+  }
+  // let checkedRating = document.querySelector('.rating.active').innerText;
 });
